e24f1e859b4fe2bf7d79fc9c7bb0ec88204d89ec
Guilherme Nunes <deoliveiranunes.guilherme@hotmail.com>
2021-04-21 21:33:44 -0300

    initial

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+
+import StartScene from './scenes/StartScene';
+import MapScene from './scenes/MapScene';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    scene: [StartScene, MapScene]
+};
+
+const game = new Phaser.Game(config);
